cca8c0934d9af3df5146751f0e4d8a066a00ef26
Lucas Penco <lucas.penco@gmail.com>
2021-12-07 20:59:28 -0300

    Skelton

---
+++ finnPenco-v1/src/main/java/br/com/madrugas/finnPencov1/FinnPencoV1Application.java
@@ -0,0 +1,13 @@
+package br.com.madrugas.finnPencov1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnPencoV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnPencoV1Application.class, args);
+	}
+
+}
---
+++ finnPenco-v1/src/test/java/br/com/madrugas/finnPencov1/FinnPencoV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.madrugas.finnPencov1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnPencoV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
