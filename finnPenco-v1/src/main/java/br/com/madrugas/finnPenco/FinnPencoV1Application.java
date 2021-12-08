285de58f7fd760daa92e3f96737c8ebe00ed75cd
Lucas Penco <lucas.penco@gmail.com>
2021-12-07 21:26:24 -0300

    Minor

--- finnPenco-v1/src/main/java/br/com/madrugas/finnPenco/FinnPencoV1Application.java
+++ finnPenco-v1/src/main/java/br/com/madrugas/finnPenco/FinnPencoV1Application.java
@@ -1,4 +1,4 @@
-package br.com.madrugas.finnPencov1;
+package br.com.madrugas.finnPenco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
